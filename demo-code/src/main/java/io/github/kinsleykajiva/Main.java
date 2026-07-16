d981fbb7e6a10d5c6be875ce27fffe4cc59ba6ee
Kinsley Kajiva <kinsleykajiva@hotmail.com>
2026-07-16 19:29:48 +0200

    init

--- demo-code/src/main/java/io/github/kinsleykajiva/Main.java
+++ demo-code/src/main/java/io/github/kinsleykajiva/Main.java
@@ -1,14 +1,12 @@
 package io.github.kinsleykajiva;
 
+import io.github.kinsleykajiva.webrtc.WebRtc;
 
 public class Main {
-	static void main() {
-		
-		System.out.println(String.format("Hello and welcome!"));
-		
-		for (int i = 1 ; i <= 5 ; i++) {
-			
-			System.out.println("i = " + i);
-		}
+	public static void main(String[] args) {
+		System.out.println("Hello and welcome!");
+
+		String status = WebRtc.probe();
+		System.out.println("Native WebRTC bridge says: " + status);
 	}
 }
